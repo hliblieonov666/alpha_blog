726d64826d78b8c09e2b89ca400d916471914ffd
hliblieonov666 <hliblieonov@gmail.com>
2019-01-13 19:57:17 +0000

    modifying articles_controlles + index + changing edit, show and new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,14 @@ class ArticlesController < ApplicationController
   
   def new
     @article = Article.new
-  
-  
+    
   end
+  
+  def edit 
+    @article = Article.find(params[:id])
+  end
+  
+  
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params) 
@@ -15,9 +20,27 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end  
+  end
+  
   def show 
     @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
   end 
+  
+  
   private 
   def article_params
   params.require(:article).permit(:title, :description)
